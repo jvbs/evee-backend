dd1aea2a4568d68553db282f176d5f01a13653c1
jvbs <jvbs98@hotmail.com>
2021-06-07 23:29:35 -0300

    ajustes build

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,4 @@
+"use strict";
 // Update with your config settings.
 module.exports = {
   development: {
@@ -38,19 +39,25 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
+  production: {
+    useNullAsDefault: true,
+    // client: "mysql",
+    // connection: {
+    //   host: "localhost",
+    //   user: "root",
+    //   password: "root",
+    //   database: "evee",
+    // },
+    client: "sqlite3",
+    connection: {
+      filename: `${__dirname}/src/database/database.sqlite`,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+  },
 };
