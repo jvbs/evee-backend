dbc74a2667a9529221d47e6209f802a67d4f77e3
jvbs <jvbs98@hotmail.com>
2021-06-14 00:03:22 -0300

    ajuste estrutura backend knex

--- knexfile.ts
+++ knexfile.ts
@@ -2,13 +2,6 @@
 module.exports = {
   development: {
     useNullAsDefault: true,
-    // client: "mysql",
-    // connection: {
-    //   host: "localhost",
-    //   user: "root",
-    //   password: "root",
-    //   database: "evee",
-    // },
     client: "sqlite3",
     connection: {
       filename: `${__dirname}/src/database/database.sqlite`,
@@ -20,6 +13,34 @@ module.exports = {
     seeds: {
       directory: `${__dirname}/src/database/seeds`,
     },
+  },
+
+  production: {
+    useNullAsDefault: true,
+    client: "sqlite3",
+    connection: {
+      filename: `${__dirname}/src/database/database.sqlite`,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+    // client: "postgresql",
+    // connection: {
+    //   database: "my_db",
+    //   user: "username",
+    //   password: "password",
+    // },
+    // pool: {
+    //   min: 2,
+    //   max: 10,
+    // },
+    // migrations: {
+    //   tableName: "knex_migrations",
+    // },
   },
 
   // staging: {
@@ -37,20 +58,4 @@ module.exports = {
   //     tableName: "knex_migrations"
   //   }
   // },
-
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
 };
