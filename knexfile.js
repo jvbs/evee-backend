67b912ca7e85ef02b8a2e6a85d6864abb72b8ae1
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2021-06-14 00:44:43 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,48 +1,48 @@
+"use strict";
 // Update with your config settings.
 module.exports = {
   development: {
     useNullAsDefault: true,
+    // client: "mysql",
+    // connection: {
+    //   host: "localhost",
+    //   user: "root",
+    //   password: "root",
+    //   database: "evee",
+    // },
     client: "sqlite3",
     connection: {
-      filename: `${__dirname}/src/database/database.sqlite`,
+      filename: __dirname + "/dist/database/database.sqlite",
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: `${__dirname}/src/database/migrations`,
+      directory: __dirname + "/dist/database/migrations",
     },
     seeds: {
-      directory: `${__dirname}/src/database/seeds`,
+      directory: __dirname + "/dist/database/seeds",
     },
   },
-
   production: {
     useNullAsDefault: true,
+    // client: "mysql",
+    // connection: {
+    //   host: "localhost",
+    //   user: "root",
+    //   password: "root",
+    //   database: "evee",
+    // },
     client: "sqlite3",
     connection: {
-      filename: `${__dirname}/src/database/database.sqlite`,
+      filename: __dirname + "/dist/database/database.sqlite",
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: `${__dirname}/src/database/migrations`,
+      directory: __dirname + "/dist/database/migrations",
     },
     seeds: {
-      directory: `${__dirname}/src/database/seeds`,
+      directory: __dirname + "/dist/database/seeds",
     },
-    // client: "postgresql",
-    // connection: {
-    //   database: "my_db",
-    //   user: "username",
-    //   password: "password",
-    // },
-    // pool: {
-    //   min: 2,
-    //   max: 10,
-    // },
-    // migrations: {
-    //   tableName: "knex_migrations",
-    // },
   },
-
   // staging: {
   //   client: "postgresql",
   //   connection: {
@@ -58,4 +58,19 @@ module.exports = {
   //     tableName: "knex_migrations"
   //   }
   // },
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
 };
