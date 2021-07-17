4fe64695230b58efd4e54f7d3e454215a62bb523
jvbs <jvbs98@hotmail.com>
2021-07-17 18:14:28 -0300

    criadas rotas prazo e tipos_trilhas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,10 +6,14 @@ import departamentoRouter from "./departamento.routes";
 import empresaRouter from "./empresa.routes";
 import usuarioRouter from "./usuario.routes";
 import authRouter from "./auth.routes";
+import tipoTrilhaRouter from "./tipo_trilha.routes";
+import prazoRouter from "./prazo.routes";
 
 const routes = Router();
 
 routes.use("/cargo", cargoRouter);
+routes.use("/prazo", prazoRouter);
+routes.use("/tipo-trilha", tipoTrilhaRouter);
 routes.use("/usuario", usuarioRouter);
 routes.use("/colaborador", colaboradorRouter);
 routes.use("/departamento", departamentoRouter);
