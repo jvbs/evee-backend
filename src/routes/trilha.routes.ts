1725f40d678ca01db6899ca07a36a5f864ed53a2
jvbs <jvbs98@hotmail.com>
2021-07-24 02:05:08 -0300

    rota edicao trilha

--- src/routes/trilha.routes.ts
+++ src/routes/trilha.routes.ts
@@ -104,6 +104,132 @@ trilhaRouter.post(
   }
 );
 
+// edição trilha
+trilhaRouter.put(
+  "/edit/:id",
+  celebrate(
+    {
+      body: Joi.object().keys({
+        trilha_id: Joi.number().required(),
+        programa: Joi.string().valid("Aprendizagem", "Estágio").required(),
+        departamento_id: Joi.number().required(),
+        empresa_id: Joi.number().required(),
+        nome: Joi.string().required(),
+        descricao: Joi.string().max(255),
+        prazo_id: Joi.number().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  async (request: Request, response: Response) => {
+    const {
+      trilha_id,
+      programa,
+      departamento_id,
+      empresa_id,
+      nome,
+      descricao,
+      prazo_id,
+    } = request.body;
+    const { id } = request.params;
+
+    // verificando se dados chaves existem
+    const checkIfTrilha = await knex("trilha").where("id", id).first();
+    const checkPrazo = await knex("prazo").where("id", prazo_id).first();
+    const checkEmpresa = await knex("empresa").where("id", empresa_id).first();
+    const checkDpto = await knex("departamento")
+      .where("id", departamento_id)
+      .first();
+
+    // caso algum deles exista
+    if (!checkDpto || !checkEmpresa || !checkPrazo || !checkIfTrilha) {
+      return response.status(400).json({
+        error: "Argumentos inválidos para a requisição.",
+      });
+    }
+
+    // // verificando se a trilha é válida
+    // const checkTrilha = await knex("trilha")
+    //   .where({
+    //     empresa_id: empresa_id,
+    //     programa: programa,
+    //     departamento_id: departamento_id,
+    //     trilha_id: trilha_id,
+    //   })
+    //   .first();
+
+    // if (checkTrilha) {
+    //   // trilha invalida
+    //   return response.status(400).json({ error: "Trilha inválida." });
+    // }
+
+    const trilha = {
+      trilha_id,
+      programa,
+      departamento_id,
+      empresa_id,
+      nome,
+      descricao,
+      prazo_id,
+      status: 1,
+    };
+
+    // console.log(trilha);
+
+    const updatedTrilha = await knex("trilha").update(trilha).where("id", id);
+
+    if (updatedTrilha) {
+      return response.status(200).json({
+        message: "Trilha atualizada com sucesso!",
+      });
+    }
+  }
+);
+
+// listagem para edicao da trilha
+trilhaRouter.get("/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  if (!id) {
+    return response.status(400).json({
+      error: "Argumentos inválidos para a requisição.",
+    });
+  }
+
+  const trilha = await knex("trilha")
+    .select("trilha.*", "tipo_trilha.nome_trilha", "prazo.nome_prazo")
+    .leftJoin("tipo_trilha", "trilha.trilha_id", "=", "tipo_trilha.id")
+    .leftJoin("prazo", "trilha.prazo_id", "=", "prazo.id")
+    .where({
+      "trilha.id": id,
+    })
+    .first();
+
+  return response.json(trilha);
+});
+
+// listagem para edicao da trilha
+trilhaRouter.get("/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  if (!id) {
+    return response.status(400).json({
+      error: "Argumentos inválidos para a requisição.",
+    });
+  }
+
+  const trilha = await knex("trilha")
+    .select("trilha.*", "tipo_trilha.nome_trilha", "prazo.nome_prazo")
+    .leftJoin("tipo_trilha", "trilha.trilha_id", "=", "tipo_trilha.id")
+    .leftJoin("prazo", "trilha.prazo_id", "=", "prazo.id")
+    .where({
+      "trilha.id": id,
+    })
+    .first();
+
+  return response.json(trilha);
+});
+
 trilhaRouter.get(
   "/aprendizagem/:id",
   async (request: Request, response: Response) => {
