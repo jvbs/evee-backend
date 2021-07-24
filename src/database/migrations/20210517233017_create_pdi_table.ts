163250f8679ef641a848df4339ae560901857a98
jvbs <jvbs98@hotmail.com>
2021-07-24 19:16:55 -0300

    criada tabela pdi

--- src/database/migrations/20210517233017_create_pdi_table.ts
+++ src/database/migrations/20210517233017_create_pdi_table.ts
@@ -3,11 +3,19 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("pdi", (table: Knex.TableBuilder) => {
     table.increments("id").primary();
-    table.string("nome_trilha", 40).notNullable();
-    table.enum("tipo_trilha", [1, 2, 3]).notNullable();
-    table.integer("duracao", 2).notNullable();
-    table.integer("empresa_id").unsigned().references("id").inTable("empresa");
+    table.integer("trilha_id").unsigned().references("id").inTable("trilha");
+    table.string("tipo_trilha", 100).notNullable();
+    table.string("nome_programa", 100).notNullable();
+    table.string("nome_trilha", 100).notNullable();
+    table
+      .integer("mentor_responsavel_id")
+      .unsigned()
+      .references("id")
+      .inTable("colaborador");
+    table.string("mentor_responsavel_nome", 100).notNullable();
+    table.string("competencias_tags", 130).notNullable();
     table.enum("status", [1, 0]);
+    table.string("avaliacao", 100).notNullable();
   });
 }
 
