057cf32be720763321aa46fa3074f60ae6d855f6
jvbs <jvbs98@hotmail.com>
2021-07-26 01:18:56 -0300

    edicao pdi

--- src/routes/pdi.routes.ts
+++ src/routes/pdi.routes.ts
@@ -9,7 +9,7 @@ const pdiRouter = Router();
 // habilitando middlewares
 pdiRouter.use(isAuthenticated);
 
-// criar nova trilha
+// criar novo pdi
 pdiRouter.post(
   "/create",
   celebrate(
@@ -89,6 +89,89 @@ pdiRouter.post(
       } else {
         console.log(error);
       }
+    }
+  }
+);
+
+// atualizar pdi
+pdiRouter.put(
+  "/edit/:id",
+  celebrate(
+    {
+      body: Joi.object().keys({
+        trilha_id: Joi.number().required(),
+        nome_programa: Joi.string().valid("Aprendizagem", "Estágio").required(),
+        competencias_tags: Joi.string().allow(null, ""),
+        mentor_responsavel_id: Joi.number().required(),
+        mentorado_id: Joi.number().required(),
+        nome_trilha: Joi.string().required(),
+        avaliacao: Joi.string().required(),
+        status: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  async (request: Request, response: Response) => {
+    const { id } = request.params;
+
+    const {
+      trilha_id,
+      nome_programa,
+      competencias_tags,
+      mentor_responsavel_id,
+      nome_trilha,
+      mentorado_id,
+      avaliacao,
+      status,
+    } = request.body;
+
+    if (!id) {
+      return response.status(400).json({
+        error: "Argumentos inválidos para a requisição.",
+      });
+    }
+
+    // verificando se dados chaves existem
+    const checkTrilha = await knex("trilha")
+      .select("trilha.*", "tipo_trilha.nome_trilha")
+      .leftJoin("tipo_trilha", "trilha.trilha_id", "=", "tipo_trilha.id")
+      .where("trilha.id", trilha_id)
+      .first();
+    const checkMentor = await knex("colaborador")
+      .select("colaborador.id", "colaborador.nome")
+      .where({ id: mentor_responsavel_id, tipo_usuario: "Mentor" })
+      .first();
+    const checkMentorado = await knex("colaborador")
+      .select("colaborador.id", "colaborador.nome")
+      .where({ id: mentorado_id, tipo_usuario: "Mentorado" })
+      .first();
+
+    // caso algum deles exista
+    if (!checkMentor || !checkTrilha || !checkMentorado) {
+      return response.status(400).json({
+        error: "Argumentos inválidos para a requisição.",
+      });
+    }
+
+    const pdi = {
+      trilha_id,
+      tipo_trilha: checkTrilha.nome_trilha,
+      nome_programa,
+      nome_trilha,
+      mentor_responsavel_id,
+      mentor_responsavel_nome: checkMentor.nome,
+      competencias_tags,
+      mentorado_id,
+      status,
+      avaliacao,
+    };
+
+    const updatedPdi = await knex("pdi").update(pdi).where("id", id);
+
+    if (updatedPdi) {
+      return response.json({
+        message: "PDI atualizado com sucesso!",
+      });
     }
   }
 );
