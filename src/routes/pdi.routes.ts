52b9980a4bfe7da2e7c10bf88caefcccd0873edb
jvbs <jvbs98@hotmail.com>
2021-07-30 00:09:15 -0300

    regra de validacao de pdi invalido

--- src/routes/pdi.routes.ts
+++ src/routes/pdi.routes.ts
@@ -57,6 +57,21 @@ pdiRouter.post(
       });
     }
 
+    const checkInvalidPdi = await knex("pdi")
+      .where({
+        mentorado_id: mentorado_id,
+        status: "Ativo",
+      })
+      .first();
+
+    // mentor tentar incluir um pdi ao mentorado na situaçao onde o mentorado ja
+    // possua um pdi ativo
+    if (checkInvalidPdi) {
+      return response.status(400).json({
+        error: "Mentorado já possui um PDI ativo.",
+      });
+    }
+
     const pdi = {
       trilha_id,
       tipo_trilha: checkTrilha.nome_trilha,
@@ -153,6 +168,21 @@ pdiRouter.put(
       });
     }
 
+    // const checkInvalidPdi = await knex("pdi")
+    //   .where({
+    //     mentorado_id: mentorado_id,
+    //     status: "Ativo",
+    //   })
+    //   .first();
+
+    // // mentor tentar incluir um pdi ao mentorado na situaçao onde o mentorado ja
+    // // possua um pdi ativo
+    // if (checkInvalidPdi) {
+    //   return response.status(400).json({
+    //     error: "Mentorado já possui um PDI ativo.",
+    //   });
+    // }
+
     const pdi = {
       trilha_id,
       tipo_trilha: checkTrilha.nome_trilha,
@@ -176,7 +206,45 @@ pdiRouter.put(
   }
 );
 
-pdiRouter.get("/:id", async (request: Request, response: Response) => {
+// listar pdi especifico do mentorado - ativo
+pdiRouter.get(
+  "/mentored/active/:id",
+  async (request: Request, response: Response) => {
+    const { id } = request.params;
+
+    if (!id) {
+      return response.status(400).json({
+        error: "Argumentos inválidos para completar a requisição.",
+      });
+    }
+
+    const selectedPdi = await knex("pdi")
+      .select(
+        "pdi.*",
+        "colaborador.foto",
+        "colaborador.tipo_usuario",
+        "trilha.descricao",
+        "prazo.nome_prazo",
+        "cargo.nome_cargo"
+      )
+      .leftJoin(
+        "colaborador",
+        "pdi.mentor_responsavel_id",
+        "=",
+        "colaborador.id"
+      )
+      .leftJoin("trilha", "pdi.trilha_id", "=", "trilha.id")
+      .leftJoin("prazo", "trilha.prazo_id", "=", "prazo.id")
+      .leftJoin("cargo", "colaborador.id", "=", "cargo.id")
+      .where({ "pdi.mentorado_id": id, "pdi.status": "Ativo" })
+      .first();
+
+    return response.json(selectedPdi);
+  }
+);
+
+// listar todos pdi especifico do mentorado
+pdiRouter.get("/mentored/:id", async (request: Request, response: Response) => {
   const { id } = request.params;
 
   if (!id) {
@@ -185,6 +253,30 @@ pdiRouter.get("/:id", async (request: Request, response: Response) => {
     });
   }
 
+  const selectedPdi = await knex("pdi")
+    .select(
+      "pdi.*",
+      "colaborador.foto",
+      "colaborador.tipo_usuario",
+      "trilha.descricao",
+      "prazo.nome_prazo"
+    )
+    .leftJoin("colaborador", "pdi.mentor_responsavel_id", "=", "colaborador.id")
+    .leftJoin("trilha", "pdi.trilha_id", "=", "trilha.id")
+    .leftJoin("prazo", "trilha.prazo_id", "=", "prazo.id")
+    .where({ "pdi.mentorado_id": id });
+
+  return response.json(selectedPdi);
+});
+
+pdiRouter.get("/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  if (!id) {
+    return response.status(400).json({
+      error: "Argumentos inválidos para completar a requisição.",
+    });
+  }
   const selectedPdi = await knex("pdi").where("id", id).first();
 
   return response.json(selectedPdi);
